e1e4251a5640f07d747e49fda3c8a5508e01a122
alexandre <alexandre.c.andreani@gmail.com>
2019-04-16 17:24:34 -0300

    update

--- ajax/e2/main.py
+++ ajax/e2/main.py
@@ -1,23 +1,10 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
-def converte_romanos(numero):
-	#coloque seu codigo aqui, exemplo 
-	romanos="MCM"	
-	return romanos
-	
-@app.route("/",methods=["GET","POST"])
+@app.route("/",methods=["GET"])
 def home():
-	if request.method == "POST":
-		response = request.form
-		print("response")
-		print(response)
-		numero=response["valor"]
-		romano=converte_romanos(numero)
-					
-		return romano	
-	elif request.method == "GET":
-		return render_template("recebe_numero.html")
+	if request.method == "GET":
+		return render_template("home.html")
 
 if __name__== "__main__":
 	app.run(host="0.0.0.0",debug= True)
