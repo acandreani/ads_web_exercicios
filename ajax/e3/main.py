2594c57cf608fcad9e3fc47ca2b401074d7dd4e2
alexandre <alexandre.c.andreani@gmail.com>
2019-04-23 04:16:28 -0300

    update

--- ajax/e3/main.py
+++ ajax/e3/main.py
@@ -1,11 +1,35 @@
 from flask import Flask, render_template, request, json
 app = Flask(__name__)
 
-@app.route("/ws/score",methods=["POST"])
+import sqlite3
+
+
+@app.route("/ws/cadastro",methods=["POST"])
 def ws_score():
 	if request.method == "POST":
 		print("teste- dado recebido")
-		print(request.get_json())
+		cadastro=request.get_json()
+		print(cadastro)
+		
+		
+		conn = sqlite3.connect('game.db')
+		cursor = conn.cursor()
+		# criando a tabela (schema)
+		cursor.execute("""
+		CREATE TABLE clientes (
+				id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+				nome TEXT NOT NULL,
+				idade INTEGER,
+				cpf     VARCHAR(11) NOT NULL,
+				email TEXT NOT NULL,
+				fone TEXT,
+				cidade TEXT,
+				uf VARCHAR(2) NOT NULL,
+				criado_em DATE NOT NULL
+		);
+		""")
+		
+		
 		
 		return json.dumps({"status":"ok"})
 
